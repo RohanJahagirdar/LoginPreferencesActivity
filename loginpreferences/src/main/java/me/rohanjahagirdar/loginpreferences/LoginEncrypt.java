fdd87b6d6cfdc344265bc9a0b7c4e9423c068260
Rohan Jahagirdar <rohan.s.jahagirdar@gmail.com>
2017-09-14 19:26:21 -0400

    Second Commit with Sample Encryption

---
+++ loginpreferences/src/main/java/me/rohanjahagirdar/loginpreferences/LoginEncrypt.java
@@ -0,0 +1,31 @@
+package me.rohanjahagirdar.loginpreferences;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.security.SecureRandom;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+
+import static android.R.attr.password;
+
+/**
+ * Created by Rohan on 9/13/2017.
+ */
+
+
+
+
--- loginpreferences/src/main/java/me/rohanjahagirdar/loginpreferences/LoginPreferences.java
+++ loginpreferences/src/main/java/me/rohanjahagirdar/loginpreferences/LoginPreferences.java
@@ -7,14 +7,17 @@ import android.content.SharedPreferences;
  * Created by Rohan on 9/13/2017.
  */
 
-public class Preferences {
+public class LoginPreferences {
 
     private SharedPreferences prefs;
     String PREFS_NAME = "";
 
-    public Preferences (Application app, String prefs_name) {
+    public LoginPreferences (Application app, String prefs_name) {
         PREFS_NAME = prefs_name;
         prefs = app.getSharedPreferences(PREFS_NAME, 0);
     }
 
+    public void encryptPassword() {
+
+    }
 }
